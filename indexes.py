b31606711fc67a013c757cf3a17b303cba713e97
shulepov_ai <shulepov_ai@magnit.ru>
2023-11-11 23:20:46 +0500

    Add Czekanowski index

--- indexes.py
+++ indexes.py
@@ -1,4 +1,5 @@
 #https://www.learndatasci.com/glossary/jaccard-similarity/
+#https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
 import numpy as np
 from pathlib import Path
 
@@ -15,25 +16,28 @@ def jaccard_binary(x,y):
     similarity = intersection.sum() / float(union.sum())
     return similarity
 
-def jaccar_pair(A,B):
+def jaccard_pair(A,B):
     A_B = list(set(A+B))
     A_value = [1 if species in A else 0 for species in A_B]
     B_value = [1 if species in B else 0 for species in A_B]
+    print("jaccard: ", jaccard_binary(A_value, B_value))
 
-    print(jaccard_binary(A_value, B_value))
-    
-A = list(map(str.lower, Path('./species/russia_ws').read_text().split('\n')))
+def chekanovsky_pair(A,B):
+    print("chekanovsky: ",(2*len(set(A).intersection(set(B)))) / (len(A) + len(B)))
+
+A = list(map(str.lower, Path('./species/canada_alaska').read_text().split('\n')))
 A = list(map(str.strip, A))
-B = list(map(str.lower, Path('./species/canada_alaska').read_text().split('\n')))
+B = list(map(str.lower, Path( './species/ne_europe').read_text().split('\n')))
 B = list(map(str.strip, B))
-C = list(map(str.lower, Path('./species/ne_europe').read_text().split('\n')))
+C = list(map(str.lower, Path('./species/russia_ws').read_text().split('\n')))
 C = list(map(str.strip, C))
 
-
-
-print('russia-canada_alaska')
-jaccar_pair(A,B)
-print('russia-europe_ne')
-jaccar_pair(A,C)
-print('canada_alaska-europe_ne')
-jaccar_pair(B,C)
+print('canada_alaska-ne_europe')
+jaccard_pair(A,B)
+chekanovsky_pair(A,B)
+print('canada_alaska-russia_ws')
+jaccard_pair(A,C)
+chekanovsky_pair(A,C)
+print('ne_europe-russia_ws')
+jaccard_pair(B,C)
+chekanovsky_pair(B,C)
